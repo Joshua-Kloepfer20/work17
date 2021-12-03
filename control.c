18998355a52e0c8c9b066bbff548d82d5cedbef3
Joshua Kloepfer <jkloepfer20@stuy.edu>
2021-12-03 11:43:06 -0500

    work

--- control.c
+++ control.c
@@ -10,16 +10,26 @@
 
 #define KEY 24016
 
+union semun {
+	int val;
+	struct semid_ds *buf;
+	unsigned short *array;
+	struct seminfo *__buff;
+};
+
 int main(int argc, char *argv[]) {
 	if (argc == 2 && strcmp(argv[1], "create") == 0) {
 		int file = open("telephone.txt", O_CREAT | O_TRUNC, 0644);
-		int semd = semget(KEY, 2, IPC_CREAT | 0644);
+		int semd = semget(KEY, 1, IPC_CREAT | 0644);
+		union semun us;
+		us.val = 1;
+		semctl(semd, 0, SETVAL, us);
 		int shmd = shmget(KEY, sizeof(int), IPC_CREAT | 0640);
 	}
 	else if (argc == 2 && strcmp(argv[1], "remove") == 0) {
 		int shmd = shmget(KEY, sizeof(int), 0);
-		int semid = semget(KEY, 1, 0);
-		semctl(semid, IPC_RMID, 0);
+		int semd = semget(KEY, 0, 0);
+		semctl(semd, IPC_RMID, 0);
 		shmctl(shmd, IPC_RMID, 0);
 	}
 	return 0;
---
+++ writing.c
@@ -0,0 +1,16 @@
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/sem.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define KEY 24016
+
+int main() {
+	
+	return 0;
+}
